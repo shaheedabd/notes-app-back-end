6eaa420ef8ba53fa33a3508e6006a321c139c15e
Syahid Abdillah <syahidabdillah96@gmail.com>
2022-08-29 22:17:13 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        // handler: () => {},
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        // handler: () => {},
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        // handler: () => {},
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        // handler: () => {},
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        // handler: () => {},
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
